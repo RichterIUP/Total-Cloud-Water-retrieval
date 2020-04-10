a4a309b56caae9d8e2bd5472a635042613037508
Philipp Richter <phi.richter@gmx.de>
2020-04-10 13:46:34 +0200

    Average microwindows

--- src/inversion.py
+++ src/inversion.py
@@ -59,8 +59,8 @@ def __retrieve_step(lm_param, loop_count, chi2, residuum):
     if s_n[1] + aux.ICE_FRACTION[-1] < 0.0 or s_n[3] + aux.RADIUS_ICE[-1] < 1.0:
         s_n[3] = aux.RADIUS_LIQUID[-1]-aux.RADIUS_ICE[-1]
         s_n[1] = -aux.ICE_FRACTION[-1]
-    if s_n[0] + aux.TAU_TOTAL_OPTICAL_DEPTH[-1] < 0.0:
-        s_n[0] = -aux.TAU_TOTAL_OPTICAL_DEPTH
+    if s_n[0] + aux.TOTAL_OPTICAL_DEPTH[-1] < 0.0:
+        s_n[0] = -aux.TOTAL_OPTICAL_DEPTH
         s_n[2] = aux.RADIUS_ICE[-1]-aux.RADIUS_LIQUID[-1]
     #elif s_n[1] + aux.ICE_FRACTION[-1] > 1.0 or s_n[2] + aux.RADIUS_LIQUID[-1] < 1.0:
     #    s_n[2] = aux.RADIUS_ICE[-1]-aux.RADIUS_LIQUID[-1]
