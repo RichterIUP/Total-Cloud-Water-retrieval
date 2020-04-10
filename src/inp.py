d3c5a12f6d578b33f551b5375e5137d1eacbead5
Philipp Richter <phi.richter@gmx.de>
2020-04-10 22:46:32 +0200

    Average microwindows

--- src/inp.py
+++ src/inp.py
@@ -137,7 +137,7 @@ OFFSET = 0.0
 '''
 Parameters for the first guess: tau_total, f_ice, reff_liq, reff_ice
 '''
-MCP = [1.66, 0.5, 5.0, 30.0]
+MCP = [0.1, 0.5, 5.0, 30.0]
 
 '''
 If this is set to true, then L-IWP searches for a file containing cloud height
