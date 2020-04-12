29750a00ccc84251207cb2b3e2c99090451ca3ca
Philipp Richter <phi.richter@gmx.de>
2020-04-12 16:58:16 +0200

    Backup

--- src/inversion.py
+++ src/inversion.py
@@ -32,13 +32,13 @@ ALPHA = 1.0
 X_PREV = [0.0, 0.0, 0.0, 0.0]
 LOOP_PREV = 0
 
-def calculate_epsilon(chi, s_n):
-    #s_n = [0.0 for ii in range(4)]
-    #s_n[0] = aux.TOTAL_OPTICAL_DEPTH[-1] - aux.TOTAL_OPTICAL_DEPTH[-2]
-    #s_n[1] = aux.ICE_FRACTION[-1] - aux.ICE_FRACTION[-2]
-    #s_n[2] = aux.RADIUS_LIQUID[-1] - aux.RADIUS_LIQUID[-2]
-    #s_n[3] = aux.RADIUS_ICE[-1] - aux.RADIUS_ICE[-2]
-    #s_n = np.array(s_n)
+def calculate_epsilon(chi):
+    s_n = [0.0 for ii in range(4)]
+    s_n[0] = aux.TOTAL_OPTICAL_DEPTH[-1] - aux.TOTAL_OPTICAL_DEPTH[-2]
+    s_n[1] = aux.ICE_FRACTION[-1] - aux.ICE_FRACTION[-2]
+    s_n[2] = aux.RADIUS_LIQUID[-1] - aux.RADIUS_LIQUID[-2]
+    s_n[3] = aux.RADIUS_ICE[-1] - aux.RADIUS_ICE[-2]
+    s_n = np.array(s_n)
     deriv_x_n =  np.transpose(np.matmul(np.array(np.transpose(np.matrix(numerical.jacobian(-2)))), s_n))
     res_linapprox = np.transpose(np.matrix(np.array(aux.RADIANCE_FTIR[:]) - (np.array(aux.RADIANCE_LBLDIS[0][-2][:]) + deriv_x_n)))
     linear_approx = np.float_(np.dot(np.matmul(np.transpose(res_linapprox), aux.S_Y_INV_MATRIX[:]), \
@@ -46,11 +46,11 @@ def calculate_epsilon(chi, s_n):
     change_of_costfunction = aux.CHI2[-1] - chi
     change_of_costfunction_for_linear_model = aux.CHI2[-1] - linear_approx
     eps = change_of_costfunction / change_of_costfunction_for_linear_model
-    log.write("{} {}\n".format(chi, linear_approx))
+    log.write("{} {} {} {} {}\n".format(aux.CHI2[-1], chi, linear_approx, change_of_costfunction, change_of_costfunction_for_linear_model))
     log.write("# epsilon = {}\n".format(eps)) 
     return eps
 
-def __retrieve_step(lm_param, loop_count, s_n):#, chi2, residuum):
+def __retrieve_step(lm_param, loop_count):#, chi2, residuum):
     '''
     Calculate the next parameters using Least Squares with Gauss-Newton/Levenberg-Marquardt.
     If the prior cost function is better than the current one, the current elements of the
@@ -530,7 +530,7 @@ def retrieve():
         
         __run_lbldis_and_derivatives()
     
-        [lm_param, cov_matrix, s_n, t_matrix_new] = __retrieve_step(lm_param, retr_loop, s_n)#, aux.CHI2[-1], aux.RESIDUUM[-1])
+        [lm_param, cov_matrix, s_n, t_matrix_new] = __retrieve_step(lm_param, retr_loop)#, aux.CHI2[-1], aux.RESIDUUM[-1])
 
         conv_test = __conv_diagnostics(retr_loop, cov_matrix)
         converged = __convergence(lm_param*10, retr_loop, conv_test)
