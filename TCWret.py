73bb229508def70e12eacaa33a90df8b2f1a798b
Philipp Richter <phi.richter@gmx.de>
2020-04-12 18:35:37 +0200

    Backup

--- TCWret.py
+++ TCWret.py
@@ -37,7 +37,7 @@ resolution_retrieval = -0.5
 
 spec = spectrum.split("/")[-1]
 
-for ii in range(2):
+for ii in range(1):
     subprocess.call(["python3", "src/main.py", spectrum, windows, "10", "0", str(resolution_only_od), str(tt), str(fi), str(rl), str(ri), "0", "0", "0", directory])
     with open("{}/{}/{}/results.dat".format(path, spectrum.split("/")[-1], directory), "r") as f:
         cont = f.readlines()
