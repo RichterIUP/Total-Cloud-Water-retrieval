7401cd0774e9aabdd2a7884c38c7707f2399d910
Philipp Richter <phi.richter@gmx.de>
2020-04-15 12:49:06 +0200

    Backup

--- TCWret.py
+++ TCWret.py
@@ -47,7 +47,7 @@ ri = [13.33, 26.67, 40, 53.33]
 fi = [0.2, 0.5, 0.8]
 fi_best = 0.5
 tt = np.array([0.2, 1.0, 3.0, 4.0])
-rt = np.array([10, 20, 30, 40])
+rt = np.array([10, 15, 20, 25, 30, 35, 40, 45])
 
 rt_y = []
 tt_y = []
@@ -76,28 +76,21 @@ ti_best = tt_best * fi_best
 
 diff_slope = []
 vals_r = []
-for ri_ii in [15, 30, 45, 60]:
-    for rl_ii in [5, 10, 15, 20]:
-    #rl_ii = rt[ii] / (2*fi_best+1)
-    #ri_ii = 3*rl_ii
-        subprocess.call(["python3", "src/main.py", spectrum, windows, "20", "1", str(resolution_only_od), str(tl_best), str(ti_best), str(rl_ii), str(ri_ii), "0", "0", "0", directory])
-        if os.path.exists("{}/{}/{}/lbldis.spec".format(path, spectrum.split("/")[-1], directory)):
-            with open("{}/{}/{}/lbldis.spec".format(path, spectrum.split("/")[-1], directory), "r") as f:
-                cont = f.readlines()
-                slope_lbldis.append(float(cont[-2]))
-                slope_ftir.append(float(cont[-1]))
-                diff_slope.append((np.abs(float(cont[-2])-float(cont[-1]))))
-                vals_r.append([rl_ii, ri_ii])
-                rt_y.append(rt[ii])
-            shutil.rmtree("{}/{}/{}".format(path, spectrum.split("/")[-1], directory)) 
+for ri_ii in range(8):
+    rl_ii = rt[ii] / (2*fi_best+1)
+    ri_ii = 3*rl_ii
+    subprocess.call(["python3", "src/main.py", spectrum, windows, "20", "1", str(resolution_only_od), str(tl_best), str(ti_best), str(rl_ii), str(ri_ii), "0", "0", "0", directory])
+    if os.path.exists("{}/{}/{}/lbldis.spec".format(path, spectrum.split("/")[-1], directory)):
+        with open("{}/{}/{}/lbldis.spec".format(path, spectrum.split("/")[-1], directory), "r") as f:
+            cont = f.readlines()
+            slope_lbldis.append(float(cont[-2]))
+            slope_ftir.append(float(cont[-1]))
+            diff_slope.append((np.abs(float(cont[-2])-float(cont[-1]))))
+            vals_r.append([rl_ii, ri_ii])
+            rt_y.append(rt[ii])
+        shutil.rmtree("{}/{}/{}".format(path, spectrum.split("/")[-1], directory)) 
 
-print(slope_lbldis)
-print(slope_ftir)
-print(diff_slope)
-idx = diff_slope.index(np.min(diff_slope))
-print(vals_r)
-print(vals_r[idx])
-'''
+
 slope_ftir_av = np.mean(slope_ftir)
 rt_best = np.interp(slope_ftir_av, np.array(slope_lbldis), rt_y)
 rl_best = rt_best / (2*fi_best+1)
@@ -118,7 +111,6 @@ if ri < 0.0:
     ri = 30.0
 
 subprocess.call(["python3", "src/main.py", spectrum, windows, "15", "0", str(resolution_only_od), str(tt), str(fi), str(rl), str(ri), "0", "0", "0", directory])
-'''
 #exit(-1)
 '''
 Start the retrieval with high resolution
