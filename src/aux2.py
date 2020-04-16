845da061a78f8d41afe5ffa95fc1d46d0dc47bd4
Philipp Richter <phi.richter@gmx.de>
2020-04-16 12:35:32 +0200

    Retrieve using slope and summed radiance

--- src/aux2.py
+++ src/aux2.py
@@ -329,8 +329,8 @@ def average(wavenumber, radiance):
     Speichere nur den Slope und den RMS ab
     '''
 
-    new_wavenumber = wavenumber[0:4]
-    new_radiance = [1e2*(new_radiance[0] - new_radiance[11])/(wavenumber[0] - wavenumber[11]), 1e4*(new_radiance[12] - new_radiance[-1])/(wavenumber[12] - wavenumber[-1]), np.sum(new_radiance[0:12]), np.sum(new_radiance[12:-1])]
+    new_wavenumber = wavenumber[0:2]
+    new_radiance = [1e2*(new_radiance[0] - new_radiance[11])/(wavenumber[0] - wavenumber[11]), 1e4*(new_radiance[12] - new_radiance[-1])/(wavenumber[12] - wavenumber[-1])]#, np.sum(new_radiance[0:12]), np.sum(new_radiance[12:-1])]
     return [np.array(new_wavenumber), np.array(new_radiance)]
         
     
