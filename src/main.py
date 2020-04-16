eb57ffa83b7e6706150aa97df85f4e82eefbd83b
Philipp Richter <phi.richter@gmx.de>
2020-04-16 21:01:15 +0200

    Backup

--- src/main.py
+++ src/main.py
@@ -100,6 +100,7 @@ def main(cl_param):
         inp.MCP[2] = rt_best / ((fact-1)*fi+1)
         inp.MCP[3] = rt_best * fact / ((fact-1)*fi+1)
 
+        inp.FORWARD = False
         inp.MCP_APRIORI = inp.MCP[:]
         inversion.retrieve()
 
