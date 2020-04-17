169ee20c193e06fbfbbf5dd497099a269fb1968e
Philipp Richter <phi.richter@gmx.de>
2020-04-17 16:11:02 +0200

    Guess radii

--- src/inversion.py
+++ src/inversion.py
@@ -123,13 +123,17 @@ def __retrieve_step(lm_param, loop_count, s_n):#, chi2, residuum):
     this_ri = np.float_(aux.RADIUS_ICE[-1]          + s_n[3])
     log.write("# s_n = [{:6.3f}, {:6.3f}, {:6.3f}, {:6.3f}]".format(np.float_(s_n[0])/inp.SCALE, np.float_(s_n[1])/inp.SCALE, np.float_(s_n[2]), inp.SCALE*np.float_(s_n[3])))
 
+    if this_tt < 0.0:
+        this_tt = 0.0
+    if this_fi < 0.0:
+        this_fi = 0.0
     
     '''
     Falls einer der Parameter kleiner als 0 ist, oder ice fraction groesser
     als 1 ist, verwerfe diese Parameter und erhoehe den Levenberg-Marquardt-
     Parameter und bestimme erneut das delta
     '''
-    while this_tt < 0.0 or this_fi < 0.0 or this_fi > 100.0 or this_rl < 1.0 or this_ri < 1.0:
+    while this_rl < 1.0 or this_ri < 1.0:#this_tt < 0.0 or this_fi < 0.0 or this_fi > 100.0 or this_rl < 1.0 or this_ri < 1.0:
         lm_param = lm_param * aux.INCREASE_LM
         if lm_param == 0.0:
             lm_param = inp.LM_INIT
