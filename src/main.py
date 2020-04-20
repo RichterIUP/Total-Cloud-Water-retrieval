f7f8c707a1b7cc83687410bbd4b2289d41d5dcdc
Philipp Richter <phi.richter@gmx.de>
2020-04-20 16:55:18 +0200

    Backup

--- src/main.py
+++ src/main.py
@@ -66,7 +66,7 @@ def main(cl_param):
         '''
         
 
-        '''
+
         inp.FORWARD = True
         apr_list = []
         counter = 0 
@@ -91,7 +91,7 @@ def main(cl_param):
         inp.MCP[1] = guess_apr.SEARCH_APR_MCP[idx][1]
         inp.MCP[2] = guess_apr.SEARCH_APR_MCP[idx][2]
         inp.MCP[3] = guess_apr.SEARCH_APR_MCP[idx][3]
-        '''
+
         #inp.MCP = [0.25, 0.25, 10, 45.]
 
         inp.FORWARD = False
