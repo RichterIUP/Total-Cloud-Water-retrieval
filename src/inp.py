e25721578f46e66478f355dda2477355e8718899
Philipp Richter <phi.richter@gmx.de>
2020-04-20 17:30:49 +0200

    Backup

--- src/inp.py
+++ src/inp.py
@@ -161,7 +161,7 @@ MCP_APRIORI = np.array(MCP[:])
 The variance of the a priori and its weighting. This will be converted
 to the S_A matrix (watch comment of LM_INIT)
 '''
-VARIANCE_APRIORI = [5.0**(-2), 5.0**(-2), (100.0)**(-2), (100.0)**(-2)]
+VARIANCE_APRIORI = [5.0**(-2), 5.0**(-2), (150.0)**(-2), (50.0)**(-2)]
 WEIGHT_APRIORI = 1.0
 
 '''
