2710b3c6012f564938d99d393ef2bec0b2d0b2ba
Philipp Richter <phi.richter@gmx.de>
2020-04-22 09:55:53 +0200

    Calc humd -25%

--- src/inp.py
+++ src/inp.py
@@ -9,8 +9,8 @@ MODELFRAMEWORK = 'LBLDIS'#'CLARRA'
 '''
 Path to the output of TCWret
 '''
-PATH = '/mnt/beegfs/user/phi.richter/OUTFOLDER_polarstern'
-RESULTS = '/mnt/beegfs/user/phi.richter/RESULTS_polarstern'
+PATH = '/mnt/beegfs/user/phi.richter/OUTFOLDER_humd1'
+RESULTS = '/mnt/beegfs/user/phi.richter/RESULTS_humd1'
 '''
 Path to the binary of LBLRTM
 '''
@@ -86,13 +86,13 @@ Number of CPU which shall be used for the calculation of the derivatives.
 This number will be decreased automatically, if less than NUM_OF_CPU
 are needed
 '''
-NUM_OF_CPU = 3
+NUM_OF_CPU = 6
 
 '''
 If the present spectrum is a testcase of Cox et al. (2016), then this should
 be true. This modifies the cloud boundaries and allows to add noise manually
 '''
-TESTCASE = False
+TESTCASE = True
 
 '''
 Convergence criterion due to Rodgers (2000):
@@ -109,7 +109,7 @@ the value from the spectrum file. If a testcase is retrieved, then positive
 values are the standard deviation of the added noise
 '''
 
-STDDEV = -1.0
+STDDEV = 0.0
 
 '''
 Possible values: %, ppmv, g/m3, g/kg
@@ -125,7 +125,7 @@ DISTURB_TEMPERATURE = 0.0
 '''
 Disturb the humidity relative to the value from the radiosonde
 '''
-DISTURB_HUMIDITY = 0.0
+DISTURB_HUMIDITY = -0.25
 
 '''
 Add an offset to the spectrum (mW/[sr * m2 * cm-1])
