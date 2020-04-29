20bc3fe94864d4107a5128ed980ddcf13d3c8788
Philipp Richter <phi.richter@iup.physik.uni-bremen.de>
2020-04-29 13:49:19 +0200

    Add fwd

---
+++ OUTFOLDER_fwd/PS.20170611_101300.nc/4_29_11_38_9_711416/.run_disort_0.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+(/home/phi.richter/radiative_transfer/lbldis/lbldis /mnt/beegfs/user/phi.richter/OUTFOLDER/PS.20170611_101300.nc/4_29_11_38_9_711416/.lbldis_0.parm 0 /mnt/beegfs/user/phi.richter/OUTFOLDER/PS.20170611_101300.nc/4_29_11_38_9_711416/.lbldisout_0) >& /mnt/beegfs/user/phi.richter/OUTFOLDER/PS.20170611_101300.nc/4_29_11_38_9_711416/.lbldislog_0.txt
