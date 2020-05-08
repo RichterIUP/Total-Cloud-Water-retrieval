b225c139e44bb96a86dbe2ecc2000d1186011981
Philipp Richter <phi.richter@gmx.de>
2020-05-08 09:53:14 +0200

    Backup

--- src/guess_apr.py
+++ src/guess_apr.py
@@ -13,24 +13,11 @@ SEARCH_APR_MCP = []
 def guess_apr(tt, fi, rl, ri):
 
     global SEARCH_APR
-    #ri = inp.MCP[2]
-    #rl = inp.MCP[3]
-    #fi = 0.5
-    #rms = []
-    #tt_y = []
-    #rt_y = []
 
-    #tl = tt*(1-fi)
-    #ti = tt*fi
-    rms = inversion.__only_fwd(tau_liq=tl, tau_ice=ti, reff_liq=rl, reff_ice=ri, filenum=int(100*tt+100*fi+100*rl+100*ri))[-2]
-    #tt_y.append(tt)
-            
-    #tt_best = tt
-
-    #tl_best = tl_best = tt_best * (1-fi)
-    #ti_best = ti_best = tt_best * fi        
-
-    
+    tl=tt*(1-fi)
+    ti=tt*fi
+    rms = inversion.__only_fwd(tau_liq=tl, tau_ice=ti, reff_liq=rl, reff_ice=ri, filenum=int(tt+fi+rl+10.*ri))[-2]
+       
     lock = threading.Lock()    
     lock.acquire()
         
