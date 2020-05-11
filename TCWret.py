988d107c5d83ee4fe9d54a8d19c0bc4ff662ba51
Philipp Richter <phi.richter@iup.physik.uni-bremen.de>
2020-05-11 15:53:55 +0200

    Backup

--- TCWret.py
+++ TCWret.py
@@ -8,11 +8,12 @@ import subprocess
 
 if __name__ == '__main__':
 
-    with open("../{}".format(sys.argv[1]), "r") as f:
-        cont = f.readlines()
+    #with open("../{}".format(sys.argv[1]), "r") as f:
+    #    cont = f.readlines()
 
-    for element in cont:
-        radiances_files = "/home/phi.richter/radiances/PS.20170611_1413.nc"#element.split(" ")[0]
-        atmospheric_prof = "/home/phi.richter/atm_prof/prof20170611_1413.nc"#element.split(" ")[1]
-        cloud_file = "/home/phi.richter/cloud_files/CLOUDS.20170611.141300.nc"#element.split(" ")[2].rstrip()
-        subprocess.call(["python3", "src/main.py", "/{}".format(radiances_files, atmospheric_prof, cloud_file), "TIR"])
+    #for element in cont:
+    radiances_files = "/home/phi.richter/radiances/PS.20170611_1413.nc"#element.split(" ")[0]
+    atmospheric_prof = "/home/phi.richter/atm_prof/prof20170611_1413.nc"#element.split(" ")[1]
+    cloud_file = "/home/phi.richter/cloud_files/CLOUDS.20170611.141300.nc"#element.split(" ")[2].rstrip()
+    subprocess.call(["python3", "src/main.py", "{}".format(radiances_files), "{}".format(atmospheric_prof), "{}".format(cloud_file), "TIR"])
+    
--- src/inp.py
+++ src/inp.py
@@ -9,8 +9,8 @@ MODELFRAMEWORK = 'LBLDIS'#'CLARRA'
 '''
 Path to the output of TCWret
 '''
-PATH = '/mnt/beegfs/user/phi.richter/OUTFOLDER_FIR'
-RESULTS = '/mnt/beegfs/user/phi.richter/RESULTS_FIR'
+PATH = '/mnt/beegfs/user/phi.richter/OUTFOLDER'
+RESULTS = '/mnt/beegfs/user/phi.richter/RESULTS'
 '''
 Path to the binary of LBLRTM
 '''
@@ -92,7 +92,7 @@ NUM_OF_CPU = 6
 If the present spectrum is a testcase of Cox et al. (2016), then this should
 be true. This modifies the cloud boundaries and allows to add noise manually
 '''
-TESTCASE = True
+TESTCASE = False
 
 '''
 Convergence criterion due to Rodgers (2000):
