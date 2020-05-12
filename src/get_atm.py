ac42d344a725af51ac3ee3472b3d75a0a7636781
Philipp Richter <phi.richter@gmx.de>
2020-05-12 10:09:34 +0200

    Test NyA

--- src/get_atm.py
+++ src/get_atm.py
@@ -96,7 +96,7 @@ def interpolate_to_altitude_grid(atm):
     '''
     alt_low = atm[1][0]
     if alt_low == 0.0:
-        alt_low = 0.05
+        alt_low = 0.18
     print(alt_low, alt_high, nlayer)
     #alt_grid = atm[1]
     alt_grid = np.geomspace(alt_low, alt_high, nlayer)
